a5b92fecadea729381b22916eec56efc23fe95d9
arshia88 <arshiabat5@gmail.com>
2026-05-04 13:52:46 +0330

    ;

--- Modules/User/routes/web.php
+++ Modules/User/routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\User\App\Http\Controllers\Front\DepositController;
 
-Route::prefix('/')->name('front.')->group(function() {
+Route::prefix('/front')->name('front.')->group(function() {
     Route::post('deposits/store', [DepositController::class,'store'])->name('deposits.store');
 });
